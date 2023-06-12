68b3aab2ccf7e78e8a053747a2d03fb4780d2246
FelipeBurboa <felipeburboa1991@gmail.com>
2023-06-12 11:59:10 -0400

    trying render

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,6 @@ require 'faker'
 
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-#User.create!(email: '1@example.com', name: 'Esteban', role: 0, password: 'password', password_confirmation: 'password')
-#User.create!(email: '2@example.com', name: 'Omar', role: 1, password: 'password', password_confirmation: 'password')
-#User.create!(email: '3@example.com', name: 'Ramon', role: 2, password: 'password', password_confirmation: 'password')
+User.create!(email: '1@example.com', name: 'Esteban', role: 0, password: 'password', password_confirmation: 'password')
+User.create!(email: '2@example.com', name: 'Omar', role: 1, password: 'password', password_confirmation: 'password')
+User.create!(email: '3@example.com', name: 'Ramon', role: 2, password: 'password', password_confirmation: 'password')
